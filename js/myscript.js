5a71c725c5130986456d0d92cf347f51603832c0
Polvora Advertising <Polvora1@Polvoras-iMac.local>
2017-05-30 16:28:37 -0700

    saving before heading out, still working on ths fancy map

--- js/myscript.js
+++ js/myscript.js
@@ -279,7 +279,7 @@ jQuery(window).resize(function(){
 function contactHeight(){
 	if ($(window).width() > 991){
 		var wh = jQuery('footer').height() + 70;
-		jQuery('#contacts').css('min-height', wh);
+		jQuery('#footer-clear').css('min-height', wh);
 	}
 	
 
